4eddca7f3cb3679e99a01fc3af22396f6c6b18ca
es-cheryomushkin <e.cheremushkin@g.nsu.ru>
2026-04-23 17:07:57 +0700

    review

--- virtual-proving-ground/plug.gd
+++ virtual-proving-ground/plug.gd
@@ -3,7 +3,7 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
     # Подключаем GUT (уже есть в addons, но для порядка)
     plug("bitwes/Gut")
-    plug("imjp94/gd-plug")  # сам gd-plug (проверка)
+#    plug("imjp94/gd-plug")  # сам gd-plug (проверка)
 
     # Здесь будут другие зависимости, когда появятся
     # plug("автор/репозиторий")
